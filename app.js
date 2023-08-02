cae8474abc6b4a246d3d57fda9592af5e3e61014
sabamoallem <moallema@oregonstate.edu>
2023-08-01 18:16:59 -0700

    welp

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
-const nodemailer = require("nodemailer"); // Import Nodemailer
+const mailgun = require("mailgun-js");
+const FormData = require("form-data");
 
 const app = express();
+const DOMAIN = "https://app.mailgun.com/app/sending/domains/sandboxd2ac3170934c49bab9e1eae8bcf8946e.mailgun.org"; 
+const mg = mailgun({ apiKey: "4e034d9e-74ee62ee", domain: DOMAIN });
 
 // Serve static files
 app.use(express.static(path.join(__dirname, "public")));
@@ -13,9 +16,9 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "homepage.html"));
 });
 
+// Handle "/paddles" route
 app.get("/paddles", (req, res) => {
-  // Handle paddles route
-  res.send("Paddles page");
+  res.sendFile(path.join(__dirname, "paddles.html"));
 });
 
 app.get("/photography", (req, res) => {
@@ -48,22 +51,13 @@ app.post("/submitOrder", (req, res) => {
     });
 });
 
-// Function to send email using Nodemailer
-async function sendEmailToSabamoa(formData) {
-  // Create a Nodemailer transporter using your email provider settings
-  let transporter = nodemailer.createTransport({
-    // Replace these options with your email provider settings
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
-    auth: {
-      user: "sabamoa.photography@gmail.com",
-      pass: "THISisFORCLASS",
-    },
-  });
-
-  // Compose the email
-  let emailText = `
+// Function to send email using Mailgun
+function sendEmailToSabamoa(formData) {
+  const data = new FormData();
+  data.append("from", "Excited User <mailgun@sandboxd2ac3170934c49bab9e1eae8bcf8946e.mailgun.org>");
+  data.append("to", "ashleen.moallem@gmail.com"); 
+  data.append("subject", "New Order");
+  data.append("text", `
     First Name: ${formData.firstName}
     Last Name: ${formData.lastName}
     Email: ${formData.email}
@@ -72,17 +66,19 @@ async function sendEmailToSabamoa(formData) {
     Organization: ${formData.organization}
 
     Note: This is your order. If it looks to your liking, please send it and wait 3-5 business days for a response.
-  `;
+  `);
 
-  let mailOptions = {
-    from: "sabamoa.photography@gmail.com", 
-    to: "sabamoa.photography@gmail.com", 
-    subject: "New Order", 
-    text: emailText, 
-  };
-
-  // Send the email
-  await transporter.sendMail(mailOptions);
+  return new Promise((resolve, reject) => {
+    mg.messages.create(DOMAIN, data, (error, body) => {
+      if (error) {
+        console.error("Error sending email:", error);
+        reject(error);
+      } else {
+        console.log("Email sent successfully:", body);
+        resolve(body);
+      }
+    });
+  });
 }
 
 module.exports = app;
